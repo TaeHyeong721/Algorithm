2e79978a732ac30c6cf66b48f812f5606e92c1ef
Jo Taehyeong <wpg.jth@gmail.com>
2024-01-17 15:36:16 +0900

    [BOJ] 10773번: 제로

---
+++ BOJ/BOJ10773.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class BOJ10773 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int k = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < k; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if (num == 0) {
+                stack.pop();
+            } else {
+                stack.push(num);
+            }
+        }
+
+        int sum = stack.stream()
+                .mapToInt(Integer::intValue)
+                .sum();
+        System.out.println(sum);
+    }
+}
